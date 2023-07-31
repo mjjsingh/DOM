9e7fc17bf61fccf267793117b4cedcf221b8cf1a
mjjsingh <jyoti1536@gmail.com>
2023-07-31 09:23:21 +0530

    1st practice code ends

--- dom.js
+++ dom.js
@@ -12,17 +12,46 @@
 //     items[i].style.backgroundColor = "#f4f4f4";
 // }
 
-var li= document.getElementsByTagName('li');
-li[1].textContent= "hello";
-li[2].style.frontWeight= "bold";
-//items[1].style.backgroundColor= "yellow";
+// var li= document.getElementsByTagName('li');
+// li[1].textContent= "hello";
+// li[2].style.frontWeight= "bold";
+// //items[1].style.backgroundColor= "yellow";
 
-for(let i=0; i<li.length; i++){
-    li[i].style.backgroundColor = "#f4f4f4";
-}
+// for(let i=0; i<li.length; i++){
+//     li[i].style.backgroundColor = "#f4f4f4";
+// }
 //Qweryslectors
-var header= document.querySelector("#main-header");
-header.style.borderBottom= "solid 4px #ccc";
+// var header= document.querySelector("#main-header");
+// header.style.borderBottom= "solid 4px #ccc";
 
 var input =document.querySelector('input');
 input.value= "Hello World"
+
+var submit=document.querySelector('input[type="submit"]');
+submit.value="SEND"
+
+var item = document.querySelector('.list-group-item');
+item.style.color = 'red';
+
+var lastitem = document.querySelector('.list-group-item:last-child');
+lastitem.style.color = 'blue';
+
+var seconditem = document.querySelector('.list-group-item:nth-child(2)');
+seconditem.style.color = 'coral';
+
+//QWERYSELECTORALL
+var titles = document.querySelectorAll('.title');
+
+console.log(titles);
+titles[0].textContent ='Hello';
+
+var odd= document.querySelectorAll('li:nth-child(odd)');
+
+for(var i=0; i<odd.length; i++){
+odd[i].style.backgroundColor ='#f4f4f4';
+}
+var even= document.querySelectorAll('li:nth-child(even)');
+
+for(var i=0; i<even.length; i++){
+even[i].style.backgroundColor ='#ccc';
+}
