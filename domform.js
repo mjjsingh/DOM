d5c913097109f5a4603fc8ca5b711ba2cabe5ceb
mjjsingh <jyoti1536@gmail.com>
2023-08-01 09:24:05 +0530

    store values

---
+++ domform.js
@@ -0,0 +1,30 @@
+// Get form element
+var form = document.querySelector('form');
+
+// Add submit event listener to form
+form.addEventListener('submit', function(event) {
+  // Prevent default form submission behavior
+  event.preventDefault();
+
+  // Get input values
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var phone = document.getElementById('phone').value;
+  var date = document.getElementById('date').value;
+  var time = document.getElementById('time').value;
+
+  // Create user object
+  var user = {
+    name: name,
+    email: email,
+    phone: phone,
+    date: date,
+    time: time
+  };
+
+  // Store user object in local storage
+  localStorage.setItem('user', JSON.stringify(user));
+
+//   // Change submit button text
+//   event.target.querySelector('input[type="submit"]').value = 'Submit';
+});
